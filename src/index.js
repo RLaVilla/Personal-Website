20c946673259cbe3993e2ec4dd24e200f6552b6e
RLaVilla <codingrlav@gmail.com>
2025-02-27 17:43:51 -0500

    Made website very responsive, down until 600px. Have to complete down to 320 px. Made dropdown menu for nav items when screen size is less then 600px.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
 import "./style.css";
-import { populateHome } from "./home.js";
-import { populateAbout } from "./about.js";
 
-// populateHome();
-
-
-
-const homeButton = document.getElementById("home");
-homeButton.addEventListener("click", () => {
-  populateHome();
-});
-
-const aboutButton = document.getElementById("about");
-aboutButton.addEventListener("click", () => {
-    populateAbout();
-})
-
-const projectsButton = document.getElementById("projects");
-
-const resumeButton = document.getElementById("resume");
-
-const contactButton = document.getElementById("contact");
-
-const githubImg = document.getElementById("github");
-githubImg.addEventListener("click", () => {
-  window.open("https://github.com/RLaVilla", "_blank");
-});
-
-const linkedInImg = document.getElementById("linkedin");
-linkedInImg.addEventListener("click", () => {
-  window.open("https://www.linkedin.com/", "_blank");
+document.getElementById("menuToggle").addEventListener("click", function () {
+  document.getElementById("nav").classList.toggle("active");
 });
